226e2b888a1a081449e0b7b3d618e8f91e40010d
root <julian@keller-heikkila.org>
2018-08-09 01:29:59 +0000

    editzzz

--- cal.cpp
+++ cal.cpp
@@ -17,13 +17,13 @@ int divide(int e, int f) {
 }
 
 int main() {
-	std::cout << "Enter the first number: ";
+	std::cout << "Enter the first number to add/subtract/multiply/divide: ";
 	int g = 0;
 	int h = 0;
 	std::cin >> g;
 	std::cout << "Enter the second number: ";
 	std::cin >> h;
 	std::cout << "+--------------------------------+" << std::endl;
-	std::cout << "|             " << add(g, h) << " Is +           |" << std::endl << "|             " << subtract(g, h) << " Is -           |" << std::endl <<  "|             " << multiply(g, h) << " Is *           |" << std::endl << "|             " << divide(g, h) << " Is /           |" << std::endl;
-	std::cout << "+--------------------------------+" << std::endl;
+	std::cout << g << " + " << h << " = " << add(g,h) << std::endl << g << " - " << h << " = " << subtract(g,h) << std::endl << g << " * " << h << " = " << multiply(g,h) << std::endl << g << " / " << h << " = " << divide(g,h) << std::endl;
+	std::cout <<  "*answers are rounded down* " << std::endl << "+--------------------------------+" << std::endl << std::endl;
 }
--- su-do.cpp
+++
@@ -1,6 +0,0 @@
-# include <iostream>
-# include <string>
-
-int main() {
-	std::cout << "sudo: unable to resolve host ip-nig.ger.fag.got" << std::endl;
-}
